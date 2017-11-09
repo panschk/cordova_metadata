0cabbea484e12207468bf9b33f5cc432237600ec
panschk <panschk@googlemail.com>
2017-11-09 09:54:49 +0100

    add elefantbrain

---
+++ memory/sign.bat
@@ -0,0 +1,3 @@
+REM cordova run android --release --keystore=D:\workspaces\javascript\cordova\memory\platforms\android\ELEFANTBRAIN-mobileapps.keystore --storePassword=ELEFANTBRAIN --alias=ELEFANTBRAIN --password=ELEFANTBRAIN
+
+REM cordova build --release
